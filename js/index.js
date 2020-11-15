6ad2d3bc9d5e5ce23ce8aca5613d59b9c38ba46f
Yaroslav <yaroslav6066@gmail.com>
2020-11-15 16:37:08 +0200

    create timer

--- js/index.js
+++ js/index.js
@@ -1,11 +1,28 @@
 const Game = (function () {
   const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'violet']
-  document.getElementById('timer')
+  const title = document.getElementById('timer')
+  const btn = document.getElementById('startBtn')
+  let time = 60
+  let myInterval = 1
+
   return {
-    start: function() {
-        this.timer()
+    start: function () {
+      if (myInterval == 1) {
+        btn.innerHTML = 'Pause'
+        myInterval = setInterval(function () {
+          time--
+          title.innerHTML = time
+        }, 1000)
+      } else {
+        btn.innerHTML = 'Start'
+        clearInterval(myInterval)
+        myInterval = 1
+      }
     },
-    timer: function() {  
+    reset: function () {
+      time = 60
+      title.innerHTML = time
+      clearInterval(myInterval)
     },
     close: function () {
       document.getElementById('modal').style.display = 'none'
@@ -15,3 +32,20 @@ const Game = (function () {
     },
   }
 })()
+
+// const Timer = function(callback, delay) {
+//   let timerId, start, remaining = delay;
+
+//   this.pause = function() {
+//       window.clearTimeout(timerId);
+//       remaining -= Date.now() - start;
+//   };
+
+//   this.resume = function() {
+//       start = Date.now();
+//       window.clearTimeout(timerId);
+//       timerId = window.setTimeout(callback, remaining);
+//   };
+
+//   this.resume();
+// };
