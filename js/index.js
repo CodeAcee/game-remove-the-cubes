55c5fef2f8f5ea7850448215c6f3b587ebcd83fd
Yaroslav <yaroslav6066@gmail.com>
2020-11-15 14:39:33 +0200

    add close and open method

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-document.getElementById("modal").style.display = "none";
+const Game = (function () {
+  const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'violet']
+  document.getElementById('timer')
+  return {
+    start: function() {
+        this.timer()
+    },
+    timer: function() {  
+    },
+    close: function () {
+      document.getElementById('modal').style.display = 'none'
+    },
+    open: function () {
+      document.getElementById('modal').style.display = 'block'
+    },
+  }
+})()
