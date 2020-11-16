fb5d8874c58cf12d629e35222589ce65c85d5bd9
Yaroslav <yaroslav6066@gmail.com>
2020-11-16 02:11:21 +0200

    add fix pause and reset btn

--- js/index.js
+++ js/index.js
@@ -1,45 +1,53 @@
-
-
-  const Game = (function () {
-  const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'violet']
+const Game = (function () {
   const title = document.getElementById('timer')
   const btn = document.getElementById('startBtn')
-  const gameArea = document.querySelector('.area__blocks')
-  
-  let time = 60
-  let myInterval = 1
+  let points = document.getElementById('points')
+  let gameArea = document.querySelector('.area__blocks')
   let score = 0
   let isGameStart = false
-
+    let time = 5
+      let myInterval = 1
   return {
     start: function () {
-      isGameStart = true;
-      if (myInterval == 1) {
-        this.renderBox()
+  
+      isGameStart = true
+      if (myInterval === 1) {
         btn.innerHTML = 'Pause'
+        this.renderBox()
         myInterval = setInterval(function () {
-          time--
-          title.innerHTML = time
+          title.innerHTML = time--
+          if (time <= -1) {
+            clearInterval(myInterval)
+          }
         }, 1000)
       } else {
-        btn.innerHTML = 'Start'
         clearInterval(myInterval)
+        btn.innerHTML = 'Start'
         myInterval = 1
       }
     },
+    endGame: function () {
+      console.log('end game')
+      this.hideBox()
+    },
     reset: function () {
+      btn.innerHTML = 'Start'
       time = 60
+      score = 0
       title.innerHTML = time
+      points.innerHTML = score
+      this.hideBox()
       clearInterval(myInterval)
     },
     close: function () {
       document.getElementById('modal').style.display = 'none'
     },
     open: function () {
-      document.getElementById('modal').style.display = 'block'
+      document.getElementById('modal').style.display = ''
     },
     renderBox: function () {
       gameArea.innerHTML = ''
+      const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'violet']
       let box = document.createElement('div')
       let boxSize = this.getrandom(30, 100)
       let gameSize = gameArea.getBoundingClientRect()
@@ -54,11 +62,12 @@
       box.style.top = this.getrandom(0, maxTop) + 'px'
       box.style.left = this.getrandom(0, maxLeft) + 'px'
       box.setAttribute('data-box', 'true')
+      box.setAttribute('class', 'box')
 
       gameArea.insertAdjacentElement('afterbegin', box)
     },
-    getrandom: function (min, max) {
-      return Math.floor(Math.random() * (max - min) + min)
+    hideBox: function () {
+      document.querySelector('.box').style.display = 'none'
     },
     handleBoxClick: function (event) {
       if (!isGameStart) {
@@ -66,8 +75,11 @@
       }
       if (event.target.dataset.box) {
         this.renderBox()
-        score++
+        points.innerHTML = score += 1
       }
+    },
+    getrandom: function (min, max) {
+      return Math.floor(Math.random() * (max - min) + min)
     },
   }
 })()
